549714249e9331d31e8c8f0ee9fa04006a1397a2
YashKaushik7824 <yashkaushik.7824@gmail.com>
2024-07-30 01:05:53 +0530

    Commit 1 by yash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import {Route,Routes} from "react-router-dom"
+import Navbar from "./Components/Navbar"
+import Home from "./pages/Home" 
 
 function App() {
   return (
-   <div>
-    <h1>APP</h1>
+   <div className="w-screen h-screen flex flex-col bg-theme-25 ">
+      <div className="bg-theme-900">
+      <Navbar/>
+      </div>
+        <Routes>
+          <Route path="/" element= {<Home/>} />
+          <Route path="/about" element= {<Home/>} />
+          <Route path="/contact" element= {<Home/>} />
+          <Route path="/energy-calculator" element= {<Home/>} />
+          <Route path="/carbon-footprint" element= {<Home/>} />
+        </Routes>
+      
+
    </div>
   );
 }
